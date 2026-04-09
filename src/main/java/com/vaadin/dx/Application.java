12594a653c5dc45da9c0575b79f2dc04a7db44fe
ugur-vaadin <ugur@vaadin.com>
2026-04-09 17:45:11 +0300

    use aura theme

--- src/main/java/com/vaadin/dx/Application.java
+++ src/main/java/com/vaadin/dx/Application.java
@@ -1,13 +1,16 @@
 package com.vaadin.dx;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 
+import com.vaadin.flow.theme.aura.Aura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Push
+@StyleSheet(Aura.STYLESHEET)
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
